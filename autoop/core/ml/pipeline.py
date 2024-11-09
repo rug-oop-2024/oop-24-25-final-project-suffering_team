5c4964c3540b642d9aefb5e767ec5d76dde88236
Richard Douma <r.j.douma.6@student.rug.nl>
2024-11-09 18:52:58 +0100

    Style change

--- autoop/core/ml/pipeline.py
+++ autoop/core/ml/pipeline.py
@@ -56,14 +56,12 @@ class Pipeline:
         self._metrics = metrics
         self._artifacts = {}
         self._split = split
-        if (
-            target_feature.type == "categorical"
-            and model.type != "classification"
-        ):
-            raise ValueError(
-                "Model type must be classification",
-                "for categorical target feature",
-            )
+        if target_feature.type == "categorical":
+            if model.type != "classification":
+                raise ValueError(
+                    "Model type must be classification",
+                    "for categorical target feature",
+                )
         if target_feature.type == "continuous" and model.type != "regression":
             raise ValueError(
                 "Model type must be regression for continuous target feature"
